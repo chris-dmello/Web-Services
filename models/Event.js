51184b06b67f4742b5f1bc93df0a544fbd8cb9f8
chris-dmello <c.d.dmello@gmail.com>
2019-10-23 21:00:52 -0500

    Updated User and Event Schema

--- models/Event.js
+++ models/Event.js
@@ -1,6 +1,12 @@
 const { Schema } = require('mongoose');
 
 const Event = new Schema({
+    room: [String],
+    organization: [String],
+    sponsors: [String],
+    eventName: [String],
+    dateOfEvent: [Date],
+    desc: String
 
 });
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,21 @@
 const { Schema } = require('mongoose');
 
-const User = new Schema({});
+const User = new Schema({
+    firstName: String,
+    lastName: String,
+    preferredName: String,
+    preferredPronoun: String,
+    year: String,
+    cellNumber: String,
+    netID: String,
+    CCID: String,
+    major: [String],
+    skills: [String],
+    interests: [String],
+    github: String,
+    discord: String,
+    linkedin: String,
+    bio: String
+});
 
 module.exports = User;
